2e4869d7be473f91b1fb0ffb6abc0d600135570a
mao <mao@chaijin.com.tw>
2025-07-24 08:25:19 +0800

    update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="UTF-8" />
-        <link rel="icon" type="image/png" href="img/001.png" />
+        <link rel="icon" type="image/png" href="img/favicon.jpg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Aurayale</title>
       </Head>
